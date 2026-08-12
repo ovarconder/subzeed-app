269c44a758182971d38c6b9ba2afdf1f664c424f
zablink <zablinkapp@gmail.com>
2026-08-12 23:15:33 +0700

    change BasePath to /

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/subzeed", // Rewrite force be /subzeed under main website overconda.space
+  //basePath: "/subzeed", // Rewrite force be /subzeed under main website overconda.space
+  basePath: "/",
   experimental: {
     serverActions: {
       bodySizeLimit: '25mb',
